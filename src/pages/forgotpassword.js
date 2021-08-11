07cf065f0d5189d609e13847aa9ba525f2608ce6
Amar-coder1998 <appstekamar@gmail.com>
2021-08-12 00:31:56 +0530

    add material ui login and registration page

--- src/pages/forgotpassword.js
+++ src/pages/forgotpassword.js
@@ -1,9 +1,41 @@
 import React, { form } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { DisplayScreen } from '../redux/actions/actions';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 const Forgetpassword=() =>{
-
+    const classes = useStyles();
 const dispatch = useDispatch();
   const allStates = useSelector((state) => state);
 
@@ -13,35 +45,41 @@ const dispatch = useDispatch();
 
   return(
     <div style={{ flex:1, display:'flex', flexDirection:'column', textAlign:"center"}}>
-      <text style={{fontSize:"30px", marginBottom:48}}> Forgot Password Page</text>
-      <form>
-        <div style={{flex:1, display:'flex', flexDirection:'row'}}>
-          <input
-            type="email" 
-            placeholder="Email-ID" 
-            style={{flex:1, display:'flex', margin:8, padding:8}}
-          />
-        </div>
-        <div style={{flex:1, display:'flex', flexDirection:'row'}}>
-          <input
-            type="password" 
-            placeholder="New Password" 
-            style={{flex:1, display:'flex', margin:8, padding:8}}
-          />
-        </div>
-        <div style={{flex:1, display:'flex', flexDirection:'row'}}>
-          <input
-            type="password" 
-            placeholder="Re-enter Password" 
-            style={{flex:1, display:'flex', margin:8, padding:8}}
-          />
-        </div>
-        <button style={{margin:8, padding:8}}
-        onClick={() => {
-                changeScreen(6)
-              }}>Forgot Password</button>
-    </form>
-  </div>
+      <Typography component="h1" variant="h5">
+          Forgot Password
+        </Typography>
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}></Grid>
+      <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+</form>
+</div>
+</Container>
+</div>
   );
 };
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,18 @@
 import React, { form, useState} from "react";
 import ReactDOM from 'react-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { DisplayScreen } from '../redux/actions/actions';
@@ -9,6 +22,39 @@ const Login = () => {
   const allStates = useSelector((state) => state);
   const [email_field, setEmail] = useState("");
   const [pass_field, setPassword] = useState("");
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  const classes = useStyles();
 
   const loginUser= (e) => {
     e.preventDefault();
@@ -26,7 +72,6 @@ const Login = () => {
 
   return(
       <div style={{ flex:1, display:'flex', flexDirection:'column', textAlign:"center"}}>
-        <text style={{fontSize:"30px", marginBottom:48}}> Login Page</text>
         <form>
         {allStates.userexists
           ?
@@ -34,31 +79,80 @@ const Login = () => {
           :
             null
         }
-          <div style={{flex:1, display:'flex', flexDirection:'row'}}>
-            <input
-              type="text" 
-              placeholder="Email ID"
-              onChange={e=>setEmail(e.target.value)}
-              required
-              style={{flex:1, display:'flex', margin:8, padding:8}}
-            />
-          </div>
-          <div style={{flex:1, display:'flex', flexDirection:'row'}}>
-            <input
-              type="text" 
-              placeholder="Password"
-              onChange={e=>setPassword(e.target.value)}
-              required
-              style={{flex:1, display:'flex', margin:8, padding:8}}
-            />
-          </div>
-          <button style={{margin:8, padding:8 }}
-          onClick={(e) => {
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            onChange={e=>setEmail(e.target.value)}
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            onChange={e=>setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={(e) => {
                 loginUser(e)
-              }} >Login</button>
+              }}
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </form>
-    </div>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+
+
   );
+  </form>
+  </div>
+  )
  };
 
 export default Login;
